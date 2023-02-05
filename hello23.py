1915f64d2ce18f570247a9cc5eb02c67b2efa83a
FINDMY <123183868+nikhil2005858@users.noreply.github.com>
2023-02-05 09:31:03 +0530

    Update hello23.py

--- hello23.py
+++ hello23.py
@@ -2,6 +2,7 @@ import streamlit  as st
 st.header(" WELCOME TO POLYTECHNIC  GOVERNMENT CHAMARAJANAGAR ")
 st.image("govtpolychamraj01.jpg")
 st.container()
+st.write("[FRIST SEM RESULTS](https://nikhil2005858-fristsem-result-1sem-5idq7n.streamlit.app/)")
 st.write("SECOND SEM RESULTS")
 button=st.button("GET RESULT")
 data=open("results1.html","r")
